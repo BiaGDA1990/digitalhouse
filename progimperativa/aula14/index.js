6c7956c65a85eab91edc42afac8a50ff8e968252
fabinrodrigues <fabiana.nunesrod@gmail.com>
2021-09-13 22:34:28 -0300

    att

---
+++ progimperativa/aula14/index.js
@@ -0,0 +1,14 @@
+let funcSoma = require('./testeArray')
+let funcSub = require('./funsub')
+let funcMulti = require('./funmulti')
+let funcDiv = require('./funcdiv')
+
+console.log('----------------------------------')
+console.log('CALCULADORA COM MOD. EXPORTS')
+console.log(funcSoma(10, 5))
+
+console.log(funcSub(10, 5))
+
+console.log(funcMulti(10, 5))
+
+console.log(funcDiv(10, 5))
