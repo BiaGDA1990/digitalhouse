b4111d47f1cf56a87631efffd60a768d0df5ef75
fabinrodrigues <fabiana.nunesrod@gmail.com>
2021-10-04 09:44:50 -0300

    front end

---
+++ progimperativa/aula21/curso.js
@@ -0,0 +1,11 @@
+/*
+Em um arquivo diferente, crie o objeto literal curso que tem como atributos: nome do curso (string), nota de aprovação (number), faltas máximas (number) e uma lista de estudantes (um array composto pelos alunos criados no passo 1).
+*/
+let Curso = {
+  nomeDoCurso: 'Nome do Curso',
+  notaAprov: 7,
+  faltasMax: 2,
+  listaEstudantes: []
+}
+
+module.exports = Curso
---
+++ progimperativa/aula21/revisao.js
@@ -0,0 +1,32 @@
+//Crie um objeto estudante que tenha como atributos: nome (string),
+//  quantidade de faltas (number) e notas (array de números).
+//  Crie um construtor para ele e importe-o como o módulo estudante
+
+//Nosso objeto estudante terá o método calcularMedia que retorna a
+// média de suas notas. Também terá um método chamado faltas,
+// que simplesmente aumenta o número de faltas em 1.
+
+// let estudante = {
+//     nome : "Fulano",
+//     faltas : 5,
+//     notas : [7,8,6,10]
+// }
+
+// console.log(estudante.nome);
+
+let EstudanteModulo = require('./estudante')
+let CursoModulo = require('./curso')
+
+CursoModulo.listaEstudantes.push(
+  new EstudanteModulo('Marcelo', 1, [7, 8, 9, 10])
+)
+CursoModulo.listaEstudantes.push(new EstudanteModulo('Maria', 1, [7, 8, 9, 10]))
+CursoModulo.listaEstudantes.push(
+  new EstudanteModulo('Larissa', 1, [7, 8, 9, 10])
+)
+
+console.log(estudante1.faltas)
+console.log(estudante1.ausente())
+console.log(estudante1.faltas)
+console.log('-------------------------------------')
+console.log(CursoModulo)
